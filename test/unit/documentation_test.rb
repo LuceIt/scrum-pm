0891fbd3f796f1fa6fc2fc2e4e9495d36bfa1f76
LuceIt <mario.defrutos@luceit.com>
2011-08-03 09:12:04 +0200

    add story points and spent hours in the user story

--- test/unit/documentation_test.rb
+++ test/unit/documentation_test.rb
@@ -1,6 +1,6 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
-class DocumentationTest < ActiveSupport::TestCase
+class DocumentationTest < Test::Unit::TestCase
 
   # Replace this with your real tests.
   def test_truth
